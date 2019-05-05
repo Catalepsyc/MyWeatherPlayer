95882ffcae1a729cbf9b61478621ead1300e4086
Nikita <Catalepsyc@yandex.ru>
2019-05-05 12:20:51 +0300

    HtmlParser  complete

--- src/HTMLParser.java
+++ src/HTMLParser.java
@@ -4,28 +4,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class HTMLParser {
 
    public static  void main(String[] args) throws IOException {
 
-        List<Article>listOfCity = new ArrayList<>();
-        List<String>FullList = new ArrayList<>();
+        List<Article>listOfCity = new ArrayList<>();//создаем ArrayList объектов типа Article
 
+            // List<String>FullList = new ArrayList<>();
 
+       //Подключаемся к странице для парсинга
        Document myDocCity = Jsoup.connect("http://otzyvoteli.ru/goroda").get();
-       //Document myDocRussia = Jsoup.connect("https://33tura.ru/goroda-rossii").get();
-
-      // Elements h1Elements = myDocEurope.getElementsByAttributeValue("class","content");
        Elements h1Elements = myDocCity.getElementsByAttributeValue("class","hot-article");
 
-
-        h1Elements.forEach (h1Element ->
+       //перебираем элементы и добавляем их в ArrayList объектов типа Article
+       h1Elements.forEach (h1Element ->
         {
             Element aElement = h1Element.child(1);
            //ng url = aElement.attr("href");
@@ -33,27 +28,47 @@ public class HTMLParser {
             listOfCity.add(new Article(title));
 
         });
+       //выводим то, что перебрали и нашли
         listOfCity.forEach(System.out::println);
-        //У нас один элимент ArrayListа listOfCity с индексом 1
-        //System.out.println(listOfCity.get(0));
-      System.out.println(listOfCity.get(0).getName());
-      //Создаем массив строк
-      // ArrayList<String> goroda = new ArrayList<String>();
-//Разбиваем спарсенную информацию на отдельные элементы
+
+        //У нас один элемент ArrayListа listOfCity с индексом 1
+
+               // System.out.println(listOfCity.get(0).getName());
+
+       //Разбиваем спарсенную информацию на отдельные элементы
        String sampleString = listOfCity.get(0).getName();
        System.out.println(sampleString);
        String[] items = sampleString.split(" ");
-       List<String> itemList = Arrays.asList(items);
-      // System.out.println(itemList.get(0));
+       List<String> itemList = new LinkedList<>(Arrays.asList(items));
+               //Arrays.asList(items);
 
 
-       System.out.println(itemList.get(0));
+       //Удаляем из списка города длиной менее 2 символов
+       for(int i = 0; i < itemList.size(); i++) {
+           String citye = itemList.get(i);
+           if (citye.length() <= 2) {
+               itemList.remove(citye);
+           }
+       }
 
+       //Выводим список городов
+       for(String city : itemList) {
+           System.out.println("city " + city);
+           //System.out.println(itemList.get(0));
 
+       }
+       //Очищаем ArrayList объектов типа Article
+       listOfCity.clear();
 
+       //Перегоняем LinkedList itemList в ArrayList объектов типа Article
+       for(int i=0; i<itemList.size(); i++) {
+           String str = itemList.get(i);
+          // System.out.println(str);
+          listOfCity.add(new Article(str));
+       }
 
-
-
+       //выводим то, что перебрали и нашли
+       listOfCity.forEach(System.out::println);
 
    }
 
